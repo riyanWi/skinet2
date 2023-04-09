7d540d2d0317b49c5a7f4ee64a49d619c5146065
Riyan Wibowo <tunggalrahayu1@gmail.com>
2023-04-09 22:35:48 +0700

    add module, paginatione, and shop page

---
+++ client/src/app/shop/shop.service.ts
@@ -0,0 +1,38 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Pagination } from '../shared/models/pagination';
+import { Product } from '../shared/models/product';
+import { ShopParams } from '../shared/models/shopParams';
+import { Brand } from '../shared/models/brand';
+import { Type } from '../shared/models/type';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ShopService {
+
+  baseurl = "https://localhost:5001/api/"
+
+  constructor(private http: HttpClient) { }
+
+  getProducts(ShopParams: ShopParams){
+    let params = new HttpParams();
+
+    if(ShopParams.brandId > 0) params = params.append('brandId', ShopParams.brandId);
+    if(ShopParams.typeId > 0) params = params.append('typeId', ShopParams.typeId);
+    params = params.append('sort', ShopParams.sort);
+    params = params.append('pageIndex', ShopParams.pageNumber);
+    params = params.append('pageSize', ShopParams.pageSize);
+
+
+    return this.http.get<Pagination<Product[]>>(this.baseurl + 'products', {params});
+  }
+
+  getBrands(){
+    return this.http.get<Brand[]>(this.baseurl + 'products/brands');
+  }
+
+  getTypes(){
+    return this.http.get<Type[]>(this.baseurl + 'products/types');
+  }
+}
